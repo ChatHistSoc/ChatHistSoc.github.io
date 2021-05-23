c7457b21db8e3da4d5b2b594b3fb4eb68643480e
KLegacy <63067799+LegacyKJ@users.noreply.github.com>
2021-05-23 09:48:15 -0400

    2021-05-18_CHS Update Blog
    Python generated pages

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,5 +1,5 @@
-var slideIndex = [1,1,1,1,1,1,1,1,1,1,1];
-var slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6", "mySlides7", "mySlides8", "mySlides9", "mySlides10", "mySlides11"]
+var slideIndex = [1,1,1,1,1,1,1,1,1,1,1,1];
+var slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6", "mySlides7", "mySlides8", "mySlides9", "mySlides10", "mySlides11", "mySlides12"]
 
 showSlides(1, 0);
 showSlides(1, 1);
@@ -12,6 +12,7 @@ showSlides(1, 7);
 showSlides(1, 8);
 showSlides(1, 9);
 showSlides(1, 10);
+showSlides(1, 11);
 
 
 function plusSlides(n, no) {
