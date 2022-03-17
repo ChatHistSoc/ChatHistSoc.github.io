72064e7ad2c80588767ea8c3a1d118f54cac5360
Katelyn Legacy-Roulston <katelyn.legacy@gmail.com>
2022-03-16 21:26:05 -0400

    PythonContent

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,5 +1,5 @@
-var slideIndex = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
-var slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6", "mySlides7", "mySlides8", "mySlides9", "mySlides10", "mySlides11", "mySlides12", "mySlides13", "mySlides14", "mySlides15", "mySlides16", "mySlides17", "mySlides18", "mySlides19", "mySlides20", "mySlides21", "mySlides22", "mySlides23", "mySlides24", "mySlides25", "mySlides26", "mySlides27", "mySlides28", "mySlides29", "mySlides30", "mySlides31", "mySlides32", "mySlides33"]
+var slideIndex = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
+var slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6", "mySlides7", "mySlides8", "mySlides9", "mySlides10", "mySlides11", "mySlides12", "mySlides13", "mySlides14", "mySlides15", "mySlides16", "mySlides17", "mySlides18", "mySlides19", "mySlides20", "mySlides21", "mySlides22", "mySlides23", "mySlides24", "mySlides25", "mySlides26", "mySlides27", "mySlides28", "mySlides29", "mySlides30", "mySlides31", "mySlides32", "mySlides33", "mySlides34"]
 
 showSlides(1, 0);
 showSlides(1, 1);
@@ -34,6 +34,7 @@ showSlides(1, 29);
 showSlides(1, 30);
 showSlides(1, 31);
 showSlides(1, 32);
+showSlides(1, 33);
 
 
 function plusSlides(n, no) {
